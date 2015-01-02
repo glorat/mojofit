ecf7689723bdcea8d15a2dd66d57a1587638b698
Kevin Tam <kevin@glorat.net>
2015-01-02 17:27:33 +0800

    Refactor UserState to observe from Loader - much more efficient

--- client/app/scripts/services/UserState.js
+++ client/app/scripts/services/UserState.js
@@ -6,6 +6,8 @@ angular.module('clientApp')
 
         var userStatus = MojoServer.getUserStatus();
 
+        var myCache = {}; // userId -> {state:String, data:Object}
+
         var usedExercises = function (data) {
             //var nameByUse = {};
             var x = _.chain(data)
@@ -50,7 +52,7 @@ angular.module('clientApp')
             return data;
         };
 
-        var handleUserData = function(dataObj, userData) {
+        var copyUserInto = function(dataObj, userData) {
             var data = dataObj.items;
             userData.revision = +dataObj.revision;
             userData.data = processData(data);
@@ -60,16 +62,15 @@ angular.module('clientApp')
             userData.showChart = true;
         };
 
-        var loadUserInto = function(userId, userData) {
-            if (userData === undefined || userId === undefined) {
-                userData = {data:[]}; // Return new
-            }
+        var loadUser = function(userId) {
+            var userData = {userId:userId}; // Brand new. TODO:Get from memcache
 
-            if (localStorageService.isSupported) {
+            if (0 && localStorageService.isSupported) {
                 var data = localStorageService.get('mydata');
                 if (data && data.items && data.userId === userId) {
-                    $log.info('Loading your '+userId+'data from local storage');
-                    handleUserData(data, userData);
+                    $log.info('Loading your '+userId+' data from local storage');
+                    copyUserInto(data, userData);
+                    $rootScope.$broadcast('UserState:stateLoaded',userData);
                     // If we determine it is up to date, no need to load from server!
                     if (userStatus.username === userId && userStatus.revision > data.revision) {
                         $log.info('but will async update from server');
@@ -77,28 +78,48 @@ angular.module('clientApp')
                     }
                     else {
                         $log.debug('Assume we do not need server refresh yet');
-                        return userData;
+                        return;
                     }
                 }
             }
 
+
+            reallyGoGet(userId, function(data) {
+                copyUserInto(data, userData);
+                $rootScope.$broadcast('UserState:stateLoaded',userData);
+            });
+        };
+
+        var reallyGoGet = function(userId, getCb) {
             $log.info('Loading user data from server for '+userId );
+            if (!myCache.hasOwnProperty(userId)) {
+                myCache[userId] = {};
+            }
+            if (myCache[userId].state === 'requesting') {
+                $log.info('Dropping duplicate get request for ' + userId);
+                return;
+            }
+
+            myCache[userId].state = 'requesting';
             $http.get('/userraw/' + userId).success(function(data) {
                 // Due to aliasing, server may have stored userId
                 // and we don't want to gen it at runtime server currently
                 data.userId = userId;
-                handleUserData(data, userData);
-
                 if (userId === userStatus.username && localStorageService.isSupported) {
                     $log.info('Saving data into local storage for '+userId );
                     localStorageService.set('mydata', data);
                 }
+
+                myCache[userId].state = 'done';
+                myCache[userId].data = data;
+                getCb(data);
             });
-            return userData; // Will get filled async
         };
 
         return {
-            loadUserInto : loadUserInto,
+            loadUser: loadUser,
+            //loadUserInto : loadUserInto,
+            //copyUserInto: copyUserInto,
             defaultExercises: defaultExercises
         };
     });
@@ -106,40 +127,58 @@ angular.module('clientApp')
 angular.module('clientApp')
     .factory('UserState', function ($http, $log, $rootScope, MojoServer, localStorageService, UserStateLoader) {
 
-        var currentUser = {userId:undefined, data:[], usedExercises:UserStateLoader.defaultExercises, revision:0};
+        var defaultUser = function(userId) {
+            return {userId:userId, data:[], usedExercises:UserStateLoader.defaultExercises, revision:0};
+        };
 
-        var myUser = {userId:undefined, data:[], usedExercises:UserStateLoader.defaultExercises, revision:0};
+        var currentUser = defaultUser(undefined);
+
+        var myUser = defaultUser(undefined);
+
+        var cloneInto = function(srcUser, tgtUser) {
+            tgtUser.userId = srcUser.userId;
+            tgtUser.revision = srcUser.revision;
+            tgtUser.data = srcUser.data;
+            tgtUser.usedExercises = srcUser.usedExercises;
+            tgtUser.workoutDates = srcUser.workoutDates;
+            tgtUser.activeDate = srcUser.activeDate;
+            tgtUser.showChart = srcUser.showChart;
+        };
 
         // Listen for userId changes so we can manage just ourself
         /*jshint unused: vars */
         $rootScope.$on('MojoServer:userStatus', function(event,data) {
             //userStatus === data; // Require
-            $log.info('UserState detected change in userStatus - Preloading state');
+            $log.info('UserState detected change in userStatus - Preloading state ' + data.username + ":" + data.revision);
             var userId = data.username;
-            UserStateLoader.loadUserInto(userId, myUser);
+            cloneInto(defaultUser(userId), myUser);
+            UserStateLoader.loadUser(userId);
 
-            if (currentUser.userId === userId) {
-                // FIXME: Need a proper cache inside UserStateLoader
-                $log.info('Dual loading state for currentUser');
-                UserStateLoader.loadUserInto(userId, currentUser);
+        });
+
+        $rootScope.$on('UserState:stateLoaded', function(event, data){
+            var userId = data.userId;
+            if (userId === currentUser.userId) {
+                $log.info('UserState received new state for currentUser ' + currentUser.userId);
+                cloneInto(data, currentUser);
+            }
+
+            if (userId === myUser.userId) {
+                $log.info('UserState received new state for myUser ' + myUser.userId);
+                cloneInto(data, myUser);
             }
         });
 
 
         var ret = {
-            loadUser : function(userId, userData) {
-                $log.warn('UserState.loadUser deprecated');
-                return UserStateLoader.loadUserInto(userId, userData);
-            },
             setCurrentUserId: function(newUserId) {
                 if (currentUser.userId !== newUserId) {
-                    currentUser.userId = newUserId;
-                    currentUser.data = [];
-                    UserStateLoader.loadUserInto(newUserId, currentUser);
+                    cloneInto(defaultUser(newUserId), currentUser);
+                    UserStateLoader.loadUser(newUserId);
                 }
             },
             reloadCurrentUser: function() {
-                UserStateLoader.loadUserInto(currentUser.userId, currentUser);
+                UserStateLoader.loadUser(currentUser.userId);
             },
             getCurrentUser:function() {
                 return currentUser;
