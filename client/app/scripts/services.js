944a276808fa3f1f396673f0ef92daa1a99882e8
Kevin Tam <kevin@glorat.net>
2015-01-13 20:13:59 +0800

    wilks calc

--- client/app/scripts/services.js
+++ client/app/scripts/services.js
@@ -40,6 +40,34 @@ angular.module('clientApp')
             'lb' : [1/2.2, 'kg']
         };
 
+
+        var maleWilks = function(kg) {
+            kg = kg < 40.0 ? 40 : kg;
+            kg = kg > 205.9 ? 205.9 : kg;
+            var a=-216.0475144;
+            var b=16.2606339;
+            var c=-0.002388645;
+            var d=-0.00113732;
+            var e=0.00000701863;
+            var f=-0.00000001291;
+            var denom = a + b*kg + c*kg*kg + d*kg*kg*kg + e*kg*kg*kg*kg + f*kg*kg*kg*kg*kg;
+            return 500/denom;
+        };
+
+        var femaleWilks = function(kg) {
+            kg = kg < 40.0 ? 40 : kg;
+            kg = kg > 150.9 ? 150.9 : kg;
+
+            var a=594.31747775582;
+            var b=-27.23842536447;
+            var c=0.82112226871;
+            var d=-0.00930733913;
+            var e=0.00004731582;
+            var f=-0.00000009054;
+            var denom = a + b*kg + c*kg*kg + d*kg*kg*kg + e*kg*kg*kg*kg + f*kg*kg*kg*kg*kg;
+            return 500/denom;
+        };
+
         var ret = {
             convert : function(from, fromUnit, toUnit) {
                 if (fromUnit === toUnit) {
@@ -51,7 +79,9 @@ angular.module('clientApp')
                 else {
                     return 0;
                 }
-            }
+            },
+            maleWilks : maleWilks,
+            femaleWilks : femaleWilks
         };
         return ret;
     });
