d494b6e3922fa0a2aa7089fcb41efdb98b37ee86
Kevin Tam <kevin@glorat.net>
2015-01-18 16:32:07 +0800

    Try to get workout logging into mobile

--- client/app/scripts/mobile/mobileapp.js
+++ client/app/scripts/mobile/mobileapp.js
@@ -5,16 +5,31 @@ angular.module('Gainstrack', [
   'mobile-angular-ui',
   'ui.bootstrap',
   'Gainstrack.controllers.Main',
-  'mojofit'
+  'mojofit',
+  'clientApp'
 ])
 
   .config(function($routeProvider) {
     $routeProvider.when('/plates', {templateUrl:'views/plates.html',  reloadOnSearch: false});
     $routeProvider.when('/', {templateUrl:'views/mobile/home.html',  reloadOnSearch: false});
+    $routeProvider.when('/track', {
+      templateUrl: 'views/track.html',
+      controller: 'TrackController',  reloadOnSearch: false
+    });
+
+    var app = document.URL.indexOf( 'http://' ) === -1 && document.URL.indexOf( 'https://' ) === -1;
+    if ( app ) {
+      // PhoneGap application
+      //MojoServer.setUrlPrefix ('http://www.gainstrack.com');
+    } else {
+      // Web page
+    }
+
+
   });
 
 angular.module('Gainstrack.controllers.Main', [])
 
   .controller('MainController', function($scope){
-
+    $scope.foo = 'bar';
   });
