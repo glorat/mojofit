df2c1bd908a47da1af49a92ec9bd1c11356b05ee
Kevin Tam <kevin@glorat.net>
2015-01-19 23:15:24 +0800

    Add more mobile pages

--- client/app/scripts/mobile/mobileapp.js
+++ client/app/scripts/mobile/mobileapp.js
@@ -12,10 +12,16 @@ angular.module('Gainstrack', [
   .config(function($routeProvider) {
     $routeProvider.when('/plates', {templateUrl:'views/plates.html',  reloadOnSearch: false});
     $routeProvider.when('/', {templateUrl:'views/mobile/home.html',  reloadOnSearch: false});
+    $routeProvider.when('/login', {templateUrl:'views/login.html', reloadOnSearch: false});
     $routeProvider.when('/track', {
       templateUrl: 'views/track.html',
       controller: 'TrackController',  reloadOnSearch: false
     });
+    $routeProvider.when('/user/:userId', {
+      templateUrl: 'views/mobile/mobileuser.html',
+      controller: 'MobileUserController',
+      controllerAs: 'user'
+    })
 
     var app = document.URL.indexOf( 'http://' ) === -1 && document.URL.indexOf( 'https://' ) === -1;
     if ( app ) {
