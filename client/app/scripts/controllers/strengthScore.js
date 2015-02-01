9f0486ca20129de3ebde0e53cf3544f4b121d079
Kevin Tam <kevin@glorat.net>
2015-02-01 19:42:42 +0800

    Stats for mobile

--- client/app/scripts/controllers/strengthScore.js
+++ client/app/scripts/controllers/strengthScore.js
@@ -6,11 +6,14 @@ angular.module('clientApp').directive('strengthScore', function () {
     scope: {user: '='},
     templateUrl: 'views/strength-score.html',
     controller: function ($scope) {
-
       $scope.$watch('user.stats', function (newVal) {
         $scope.data = newVal.strengthScore;
       }, false);
-
     }
   };
 });
+
+angular.module('clientApp')
+  .controller('ScoreController', function ($scope, UserState) {
+    $scope.user = UserState.getMyState();
+  });
