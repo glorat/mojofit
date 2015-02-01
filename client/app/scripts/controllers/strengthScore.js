7fdf632ca3255ed9065c29a93af6bd7c3e8f38c2
Kevin Tam <kevin@glorat.net>
2015-02-01 23:10:53 +0800

    Incentive to add bodyweight

--- client/app/scripts/controllers/strengthScore.js
+++ client/app/scripts/controllers/strengthScore.js
@@ -5,15 +5,31 @@ angular.module('clientApp').directive('strengthScore', function () {
     restrict: 'E',
     scope: {user: '='},
     templateUrl: 'views/strength-score.html',
-    controller: function ($scope) {
+    controller: function ($scope, MojoServer, WorkoutState, UserState, $location) {
+      $scope.userStatus = MojoServer.getUserStatus();
+
       $scope.$watch('user.stats', function (newVal) {
         $scope.data = newVal.strengthScore;
       }, false);
+
+
+      $scope.editWeight = function() {
+        var dt = _.last($scope.user.data).date;
+        WorkoutState.setWeightDate(dt);
+        $location.path('/trackweight');
+      }
+
+      $scope.canEdit = function() {
+        return ($scope.user.userId === $scope.userStatus.id) || ($scope.user.userId === $scope.userStatus.username);
+      };
+
+      $scope.noWeight = ! _.last($scope.user.data).body.weight;
     }
   };
 });
 
 angular.module('clientApp')
-  .controller('ScoreController', function ($scope, UserState) {
+  .controller('ScoreController', function ($scope) {
     $scope.user = UserState.getMyState();
+
   });
