20fd43059fdf4cf66e9c14802a057de49e9d398a
Kevin Tam <kevin@glorat.net>
2015-02-15 22:53:11 -0500

    Fix badges

--- client/app/scripts/services/fitViewProcessor.js
+++ client/app/scripts/services/fitViewProcessor.js
@@ -132,12 +132,8 @@
         userData.stats = {};
         userData.stats.repMax = RepMaxCalculator.genRepMaxFull(userData.data, userData.usedExercises, dataObj.prefs.preferredUnit || 'kg');
         userData.stats.strengthScore = RepMaxCalculator.calcScores(userData.data, userData.stats.repMax, UnitConverter, userData.prefs.gender);
-
-      for (var i=0; i<100; i++) {
         userData.stats.strengthHistory = RepMaxCalculator.calcScoreHistoryTable(userData, UnitConverter);
-
-      }
-        userData.setBadges = createSetBadgeMap(userData.data, userData.repMax);
+        userData.setBadges = createSetBadgeMap(userData.data, userData.stats.repMax);
     };
 
     return {
