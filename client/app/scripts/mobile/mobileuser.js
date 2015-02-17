5e301b997055c250265da3f5be2137bfd974fe80
Kevin Tam <kevin@glorat.net>
2015-02-17 13:44:08 -0500

    BIG commit. Gave fine grained scoring... but needed to up to Angular 1.3 to fix a date format issue, and squash all sorts of other date display bugs

--- client/app/scripts/mobile/mobileuser.js
+++ client/app/scripts/mobile/mobileuser.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('clientApp')
-  .controller('MobileUserController', function ($scope, $http, MojoServer, $routeParams, UserState, WorkoutState, $location) {
+  .controller('MobileUserController', function ($scope, $http, MojoServer, $routeParams, UserState, WorkoutState, $location, $filter, SetupSharedUserController) {
     $scope.userId = $routeParams.userId;
     $scope.userState = UserState.getCurrentUser();
     UserState.setCurrentUserId($scope.userId);
@@ -13,34 +13,6 @@ angular.module('clientApp')
     $scope.userStatus = MojoServer.getUserStatus();
     $scope.maxPagesSize = 4;
 
-    $scope.showChart = function(){
-      return $scope.userState.data.length > 0;
-    };
-
-    $scope.datestr = function(ts) {
-      return new Date(ts).toDateString();
-    };
-
-    $scope.canEdit = function() {
-      return ($scope.userState.userId === $scope.userStatus.id) || ($scope.userState.userId === $scope.userStatus.username);
-    };
-
-    $scope.addWorkout = function() {
-      var d = new Date($scope.newWorkout.date);
-      var utcDate = Date.UTC(d.getFullYear(), d.getMonth(), d.getDate());
-      $scope.editWorkout({date:  utcDate});
-    };
-
-    $scope.editWorkout = function(date) {
-      WorkoutState.setDate(date);
-      $location.path('/track');
-    };
-
-    $scope.editWeight = function(date) {
-      if ($scope.canEdit()) {
-        WorkoutState.setWeightDate(date);
-        $location.path('/trackweight');
-      }
-    };
+    SetupSharedUserController.setup($scope, $filter, WorkoutState, $location);
 
   });
