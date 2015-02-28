a6bb3df6c41988af0e2111088cc859685ef053ec
Kevin Tam <kevin@glorat.net>
2015-02-28 15:46:40 +0800

    Make increments work

--- client/test/program/teststronglifts.js
+++ client/test/program/teststronglifts.js
@@ -15,9 +15,35 @@ describe('Stronglifts', function() {
 
       assert.equal(3, neww.actions.length);
       assert.equal('Barbell Squat', neww.actions[0].name);
+      assert.equal('Barbell Bench Press', neww.actions[1].name);
       assert.equal(5, neww.actions[0].sets.length);
       assert.equal(5, neww.actions[0].sets[0].reps);
       assert.equal(20, neww.actions[0].sets[0].weight);
+
+      state.data.unshift(neww);
+
+      x = sl.chooseWorkout(state);
+      assert.equal('B',x);
+      neww = sl.applyWorkout(x, state);
+      assert.equal(3, neww.actions.length);
+      assert.equal('Barbell Squat', neww.actions[0].name);
+      assert.equal('Barbell Deadlift', neww.actions[2].name);
+      assert.equal(5, neww.actions[0].sets.length);
+      assert.equal(5, neww.actions[0].sets[0].reps);
+      assert.equal(20, neww.actions[0].sets[0].weight);
+
+      state.data.unshift(neww);
+      x = sl.chooseWorkout(state);
+      assert.equal('A',x);
+      neww = sl.applyWorkout(x, state);
+
+      assert.equal(3, neww.actions.length);
+      assert.equal('Barbell Squat', neww.actions[0].name);
+      assert.equal('Barbell Bench Press', neww.actions[1].name);
+      assert.equal(5, neww.actions[0].sets.length);
+      assert.equal(5, neww.actions[0].sets[0].reps);
+      assert.equal(25, neww.actions[0].sets[0].weight);
+
     });
   });
 
