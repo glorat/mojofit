2debd44f409fea70678c70e4cdc83b6a7c9d6239
Kevin Tam <kevin@glorat.net>
2015-02-28 13:27:20 +0800

    Improve e2e testing of weight editing

--- client/test/e2e/anon.js
+++ client/test/e2e/anon.js
@@ -29,7 +29,7 @@ describe('anonymous first time user', function() {
         all(by.repeater('set in action.sets')).
         get(0);
     firstSet.element(by.model('set.reps')).sendKeys(5);
-    firstSet.element(by.model('set.weight')).sendKeys(20);
+    firstSet.element(by.model('input.weight')).sendKeys(20);
     // Hacky way to find the add button. This could break
     var addSet = firstSet.all(by.className('btn-default')).get(0);
     addSet.click();
@@ -40,6 +40,17 @@ describe('anonymous first time user', function() {
     var dataRepeat = element.all(by.repeater('i in userState.data'));
     //expect(dataRepeat.isPresent()).toBe(true);
     expect(dataRepeat.count()).toBe(1);
+
+    // expect(by.id('strength-score-no-weight').visible).toBe(1); // FIXME: How to express this?
+    element(by.id('strength-score-fix-weight')).click();
+    element(by.model('input.weight')).sendKeys(75);
+    element(by.id('submitWeight')).click();
+    // This takes us back to our log
+    dataRepeat = element.all(by.repeater('i in userState.data'));
+    expect(dataRepeat.count()).toBe(1);
+    // expect(by.id('strength-score-no-weight').visible).toBe(0); // FIXME: How to express this?
+
+
   });
 
   it('should be able to start registration', function () {
