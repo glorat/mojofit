ce1eb08ecc74315c948b14446a11649dd8d1e7f8
Kevin Tam <kevin@glorat.net>
2015-02-28 15:51:08 +0800

    Test for repeating a weight

--- client/test/program/teststronglifts.js
+++ client/test/program/teststronglifts.js
@@ -31,6 +31,7 @@ describe('Stronglifts', function() {
       assert.equal(5, neww.actions[0].sets.length);
       assert.equal(5, neww.actions[0].sets[0].reps);
       assert.equal(20, neww.actions[0].sets[0].weight);
+      assert.equal(40, neww.actions[2].sets[0].weight);
 
       state.data.unshift(neww);
       x = sl.chooseWorkout(state);
@@ -43,6 +44,21 @@ describe('Stronglifts', function() {
       assert.equal(5, neww.actions[0].sets.length);
       assert.equal(5, neww.actions[0].sets[0].reps);
       assert.equal(25, neww.actions[0].sets[0].weight);
+      // Let's fail a rep already
+      neww.actions[0].sets[4].reps = 4;
+
+      state.data.unshift(neww);
+      x = sl.chooseWorkout(state);
+      assert.equal('B',x);
+      neww = sl.applyWorkout(x, state);
+      assert.equal(3, neww.actions.length);
+      assert.equal('Barbell Squat', neww.actions[0].name);
+      assert.equal('Barbell Deadlift', neww.actions[2].name);
+      assert.equal(5, neww.actions[0].sets.length);
+      assert.equal(5, neww.actions[0].sets[0].reps);
+      assert.equal(25, neww.actions[0].sets[0].weight); // SQ stayst he same
+      assert.equal(45, neww.actions[2].sets[0].weight); // DL went up though
+
 
     });
   });
