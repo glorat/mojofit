2186f888fafd104b85e68077d29088bd9c2716af
Kevin Tam <kevin@glorat.net>
2015-03-02 23:19:44 +0800

    Fix stronglifts and its tests, particulary for SQ incr and DL sets

--- client/test/program/teststronglifts.js
+++ client/test/program/teststronglifts.js
@@ -10,8 +10,8 @@ describe('Stronglifts', function() {
     it('should be a registered program', function() {
       assert.ok(_.contains(ProgramRegistry.listPrograms(), 'Stronglifts 5x5'));
       var avail = ProgramRegistry.listWorkouts('Stronglifts 5x5');
-      assert.equal('A', avail[0]);
-      assert.equal('B', avail[1]);
+      assert.equal(avail[0], 'A');
+      assert.equal(avail[1], 'B');
     });
   });
 
@@ -23,37 +23,37 @@ describe('Stronglifts', function() {
       assert.equal('A',x);
       var neww = sl.applyWorkout(x, state);
 
-      assert.equal(3, neww.actions.length);
-      assert.equal('Barbell Squat', neww.actions[0].name);
-      assert.equal('Barbell Bench Press', neww.actions[1].name);
-      assert.equal(5, neww.actions[0].sets.length);
-      assert.equal(5, neww.actions[0].sets[0].reps);
-      assert.equal(20, neww.actions[0].sets[0].weight);
+      assert.equal(neww.actions.length, 3);
+      assert.equal(neww.actions[0].name, 'Barbell Squat');
+      assert.equal(neww.actions[1].name, 'Barbell Bench Press');
+      assert.equal(neww.actions[0].sets.length, 5);
+      assert.equal(neww.actions[0].sets[0].reps, 5);
+      assert.equal(neww.actions[0].sets[0].weight, 20);
 
       state.data.unshift(neww);
 
       x = sl.chooseWorkout(state);
       assert.equal('B',x);
       neww = sl.applyWorkout(x, state);
-      assert.equal(3, neww.actions.length);
+      assert.equal(neww.actions.length, 3);
       assert.equal('Barbell Squat', neww.actions[0].name);
       assert.equal('Barbell Deadlift', neww.actions[2].name);
-      assert.equal(5, neww.actions[0].sets.length);
-      assert.equal(5, neww.actions[0].sets[0].reps);
-      assert.equal(20, neww.actions[0].sets[0].weight);
-      assert.equal(40, neww.actions[2].sets[0].weight);
+      assert.equal(neww.actions[0].sets.length, 5);
+      assert.equal(neww.actions[0].sets[0].reps, 5);
+      assert.equal(neww.actions[0].sets[0].weight, 22.5, 'SQ goes up');
+      assert.equal(neww.actions[2].sets[0].weight, 40, 'DL on init');
 
       state.data.unshift(neww);
       x = sl.chooseWorkout(state);
       assert.equal('A',x);
       neww = sl.applyWorkout(x, state);
 
-      assert.equal(3, neww.actions.length);
+      assert.equal(neww.actions.length, 3);
       assert.equal('Barbell Squat', neww.actions[0].name);
       assert.equal('Barbell Bench Press', neww.actions[1].name);
-      assert.equal(5, neww.actions[0].sets.length);
-      assert.equal(5, neww.actions[0].sets[0].reps);
-      assert.equal(25, neww.actions[0].sets[0].weight);
+      assert.equal(neww.actions[0].sets.length, 5);
+      assert.equal(neww.actions[0].sets[0].reps, 5);
+      assert.equal(neww.actions[0].sets[0].weight, 25, 'SQ goes up again');
       // Let's fail a rep already
       neww.actions[0].sets[4].reps = 4;
 
@@ -64,10 +64,10 @@ describe('Stronglifts', function() {
       assert.equal(3, neww.actions.length);
       assert.equal('Barbell Squat', neww.actions[0].name);
       assert.equal('Barbell Deadlift', neww.actions[2].name);
-      assert.equal(5, neww.actions[0].sets.length);
-      assert.equal(5, neww.actions[0].sets[0].reps);
-      assert.equal(25, neww.actions[0].sets[0].weight); // SQ stayst he same
-      assert.equal(45, neww.actions[2].sets[0].weight); // DL went up though
+      assert.equal(neww.actions[0].sets.length, 5);
+      assert.equal(neww.actions[0].sets[0].reps, 5);
+      assert.equal(neww.actions[0].sets[0].weight, 25, 'SQ stays the same');
+      assert.equal(neww.actions[2].sets[0].weight, 45, 'DL went up anyway');
 
 
     });
