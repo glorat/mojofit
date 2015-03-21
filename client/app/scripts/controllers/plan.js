0e94f8dd668adaf93af802539678988039977ad8
Kevin Tam <kevin@glorat.net>
2015-03-21 14:01:47 +0800

    Make the plan nice

--- client/app/scripts/controllers/plan.js
+++ client/app/scripts/controllers/plan.js
@@ -39,23 +39,45 @@ angular.module('clientApp')
       $scope.workoutStatus = MojoServer.deleteWorkout($scope.editWorkout.date, deleteCB);
     };
 
-    $scope.$watch('workout.program.id', function(){
-      $scope.workoutNames = ProgramRegistry.listWorkouts($scope.workout.program);
-      var program = ProgramRegistry.getProgram($scope.workout.program);
-      if (program) {
+
+    $scope.programNames = ProgramRegistry.listPrograms();
+
+    $scope.$watch('workout.program.id', function(newVal){
+      $scope.workoutNames = ProgramRegistry.listWorkouts(newVal);
+      var program = ProgramRegistry.getProgram(newVal);
+      if (program && !$scope.workout.program.workout) {
+        // Auto select a program for the user
+        $scope.workout.program.workout =  program ? program.chooseWorkout($scope.user) : '';
+
         $scope.paramSchema = program.paramSchemaByField();
         var defParam = program.defaultParam($scope.user, 'kg');
         var param = program.genParams($scope.user, defParam);
-        $scope.param = param;
+
+        // Update display
         $scope.paramKeys = program.paramKeys();
         $scope.programName = program.name;
-        $scope.workout.program = program.id;
-        $scope.workout.workout =  program ? program.chooseWorkout($scope.user) : '';
-      }
-      else {
-        // ???
-      }
 
+        // Update model
+        $scope.workout.program.id = program.id;
+        $scope.workout.program.workout =  program ? program.chooseWorkout($scope.user) : '';
+        $scope.workout.program.param = param;
+
+      }
     });
+
+    $scope.applyWorkout = function() {
+      if ($scope.workout.actions.length==0 || window.confirm('Apply ' + $scope.workout.program.id + ' exercises for workout ' + $scope.workout.program.workout + '?')) {
+        var program = ProgramRegistry.getProgram($scope.workout.program.id);
+
+        // FIXME: Grab param (and unit) from prefs
+        var param = $scope.workout.program.param || program.defaultParam($scope.user, 'kg');
+
+        var wout = program.applyWorkout($scope.workout.workout, $scope.user, $scope.workout.date, param);
+        $scope.workout.actions = wout.actions;
+
+        // Hide the params now
+        $scope.paramCollapsed = 1;
+      }
+    };
 
   });
