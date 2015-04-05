fb7b94f0c69b88836e49da8e3710d267071b746a
Kevin Tam <kevin@glorat.net>
2015-04-06 01:25:29 +0800

    Extract workout panel

--- client/app/scripts/controllers/user.js
+++ client/app/scripts/controllers/user.js
@@ -28,32 +28,6 @@ angular.module('clientApp')
       $scope.userState = UserState.getCurrentUser();
       $scope.userLoadState = UserState.userLoadState;
 
-      $scope.scoreFor = function(date, exname) {
-        // TODO: Pull this from strengthHistory.cols
-        var INDICES = ['foo Date', 'Barbell Squat','Barbell Bench Press','Barbell Deadlift'];
-        var user = $scope.userState;
-        var exIndex = _.indexOf(INDICES, exname);
-        if (exIndex<0) {return '';}
-        var item = _.find(user.data, function(it){return it.date.valueOf() === date.valueOf();});
-        var itemIndex = item.index;
-        var before = user.data[itemIndex+1];
-        var after = user.data[itemIndex];
-        var beforeScore = (before && before.scores) ? before.scores[exIndex] : 0;
-        var afterScore = (after && after.scores) ? after.scores[exIndex] : 0;
-        var gain = afterScore-beforeScore;
-        /*if (gain < 0.00000001 && afterScore===0) {
-         return 'WTF';
-         }*/
-
-        if (gain < 0.0005)
-        {
-          return '';
-          //$filter('number')(afterScore*1000,0);
-        }
-        else {
-          return '+' + $filter('number')(gain*1000,0) + ' pts';
-        }
-      };
       $scope.showChart = function(){
         return $scope.userState.data.length > 0;
       };
@@ -72,15 +46,7 @@ angular.module('clientApp')
 
       $scope.editWorkout = function(date) {
         WorkoutState.setUtcDate(date);
-        $location.
-          path('/track');
-      };
-
-      $scope.editWeight = function(date) {
-        if ($scope.canEdit()) {
-          WorkoutState.setWeightDate(date);
-          $location.path('/trackweight');
-        }
+        $location.path('/track');
       };
     };
 
