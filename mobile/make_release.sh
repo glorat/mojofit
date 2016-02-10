a409cba60d93eb92b7397dbc1f5d1761df125c74
Kevin Tam <kevin@glorat.net>
2016-02-10 15:12:12 +0800

    Update for Cordova 6.0.0

--- mobile/make_release.sh
+++ mobile/make_release.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+# Make sure you upped the version in config.xml
 rm Gainstrack.apk
 cordova build --release android
-jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ../conf/gainstrack.keystore platforms/android/ant-build/CordovaApp-release-unsigned.apk gainstrack
-/Users/kevin/Library/Android/sdk//build-tools/21.1.2/zipalign -v 4 platforms/android/ant-build/CordovaApp-release-unsigned.apk Gainstrack.apk
+jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ../conf/gainstrack.keystore platforms/android/build/outputs/apk/android-release-unsigned.apk gainstrack
+/Users/kevin/Library/Android/sdk//build-tools/21.1.2/zipalign -v 4 platforms/android/build/outputs/apk/android-release-unsigned.apk Gainstrack.apk
 
